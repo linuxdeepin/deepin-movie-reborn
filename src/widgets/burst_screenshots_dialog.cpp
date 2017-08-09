2891b6b29144c58f14c972bf8c87c63c697c9c10
Sian Cao <yinshuiboy@gmail.com>
2017-08-09 15:48:55 +0800

    burst shooting: Adjust details
    Change-Id: Iae39542cb9f6856ee571e9b8ec9c418fbc5e481b

--- src/widgets/burst_screenshots_dialog.cpp
+++ src/widgets/burst_screenshots_dialog.cpp
@@ -4,7 +4,6 @@
 DWIDGET_USE_NAMESPACE
 
 namespace dmr {
-
 BurstScreenshotsDialog::BurstScreenshotsDialog(PlayerEngine* mpv)
     :DDialog(nullptr), _engine(mpv)
 {
@@ -13,23 +12,42 @@ BurstScreenshotsDialog::BurstScreenshotsDialog(PlayerEngine* mpv)
     auto *ml = new QVBoxLayout;
     ml->setContentsMargins(0, 0, 0, 0);
 
+    // top 
     auto *hl = new QHBoxLayout();
+    hl->setSpacing(16);
     ml->addLayout(hl);
 
     auto *pm = new QLabel(this);
-    pm->setPixmap(QPixmap(":/resources/icons/logo.svg"));
+    pm->setPixmap(QPixmap(":/resources/icons/logo-big.svg").scaled(44, 44));
     hl->setAlignment(pm, Qt::AlignCenter);
     hl->addWidget(pm);
 
+    // top right up
+    auto *trl = new QVBoxLayout;
+    hl->setContentsMargins(21, 0, 0, 0);
+    hl->setSpacing(6);
+    hl->addLayout(trl, 1);
+
     auto *nm = new QLabel(QFileInfo(mi.filePath).fileName(), this);
-    hl->setAlignment(nm, Qt::AlignCenter);
-    hl->addWidget(nm);
+    trl->addWidget(nm);
+
+    //top right bottom
+    auto *trb = new QGridLayout;
+    trb->addWidget(new QLabel(tr("duration: %1").arg(mi.durationStr()), this), 0, 0);
+    trb->addWidget(new QLabel(tr("resolution: %1").arg(mi.resolution), this), 0, 1);
+    trb->addWidget(new QLabel(tr("size: %1").arg(mi.sizeStr()), this), 0, 2);
+    trl->addLayout(trb);
+
+
 
     _grid = new QGridLayout();
+    _grid->setHorizontalSpacing(12);
+    _grid->setVerticalSpacing(15);
+    _grid->setContentsMargins(21, 0, 0, 0);
     ml->addLayout(_grid);
-    _grid->setColumnMinimumWidth(0, 100);
-    _grid->setColumnMinimumWidth(1, 100);
-    _grid->setColumnMinimumWidth(2, 100);
+    _grid->setColumnMinimumWidth(0, 160);
+    _grid->setColumnMinimumWidth(1, 160);
+    _grid->setColumnMinimumWidth(2, 160);
 
 
     QWidget  *mainContent = new QWidget;
@@ -39,6 +57,22 @@ BurstScreenshotsDialog::BurstScreenshotsDialog(PlayerEngine* mpv)
     connect(_engine, &PlayerEngine::notifyScreenshot, this, &BurstScreenshotsDialog::OnScreenshot);
 }
 
+static QPixmap makeRounded(QPixmap pm)
+{
+    QPixmap dest(pm.size());
+
+    QPainter p(&dest);
+    p.setRenderHints(QPainter::Antialiasing|QPainter::SmoothPixmapTransform);
+
+    QPainterPath path;
+    path.addRoundedRect(QRect(QPoint(), pm.size()), 2, 2);
+    p.setClipPath(path);
+
+    p.drawPixmap(0, 0, pm);
+
+    return dest;
+}
+
 void BurstScreenshotsDialog::OnScreenshot(const QImage& frame)
 {
     qDebug() << __func__ << _count;
@@ -46,12 +80,14 @@ void BurstScreenshotsDialog::OnScreenshot(const QImage& frame)
         return;
     }
 
-    auto scaled = frame.scaled(170, 100, Qt::KeepAspectRatio);
-    //scaled.save(QString("debug-%1.jpg").arg(QTime::currentTime().msec()));
-    auto *l = new QLabel(this);
+    auto scaled = frame.scaled(178, 100, Qt::KeepAspectRatio);
+    auto *l = new ThumbnailFrame(this);
+
     int r = _count / 3;
     int c = _count % 3;
-    l->setPixmap(QPixmap::fromImage(scaled));
+
+    auto pm = makeRounded(QPixmap::fromImage(scaled));
+    l->setPixmap(pm);
     _grid->addWidget(l, r, c);
     _count++;
     if (_count >= 15) {
--- src/widgets/burst_screenshots_dialog.h
+++ src/widgets/burst_screenshots_dialog.h
@@ -9,7 +9,26 @@ DWIDGET_USE_NAMESPACE
 namespace dmr {
 class PlayerEngine;
 
+class ThumbnailFrame: public QLabel { 
+    Q_OBJECT
+public:
+    ThumbnailFrame(QWidget* parent) :QLabel(parent) {
+        setStyleSheet(
+                "dmr--ThumbnailFrame {"
+                "border-radius: 4px;"
+                "border: 1px solid rgba(255, 255, 255, 0.1); }");
+        auto e = new QGraphicsDropShadowEffect(this);
+        //box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+        e->setColor(qRgba(0, 0, 0, 50));
+        e->setOffset(0, 2);
+        e->setBlurRadius(4);
+        setGraphicsEffect(e);
+    }
+};
+
+
 class BurstScreenshotsDialog: public DDialog {
+    Q_OBJECT
 public:
     BurstScreenshotsDialog(PlayerEngine*);
 
