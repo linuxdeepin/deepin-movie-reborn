4c5fb4d9905199468d2feacec5288a5154833c4f
tanghaixiang <tanghaixiang@uniontech.com>
2020-04-10 18:07:52 +0800

    Type:fix
    Description:音乐不会显示信息和列表里面的缩略图
    
    RootCause:Null
    
    Solution:增加该缩略图现实功能

--- src/libdmr/player_engine.cpp
+++ src/libdmr/player_engine.cpp
@@ -73,6 +73,7 @@ PlayerEngine::PlayerEngine(QWidget *parent)
 
     setLayout(l);
 
+
 #ifndef _LIBDMR_
     connect(&Settings::get(), &Settings::subtitleChanged, this, &PlayerEngine::updateSubStyles);
 #endif
@@ -783,7 +784,10 @@ void PlayerEngine::resizeEvent(QResizeEvent *re)
         clearMask();
     }
 #endif
+
 }
+
+
 
 void PlayerEngine::setBackendProperty(const QString &name, const QVariant &val)
 {
