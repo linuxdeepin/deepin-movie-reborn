b570f3841a0bc7bd8331fcb15bde8b1c6f21af29
tanghaixiang <tanghaixiang@uniontech.com>
2020-04-21 22:28:14 +0800

    Type:fix
    Description:wayland检测
    
    RootCause:Null
    
    Solution:wayland检测

--- src/backends/mpv/mpv_proxy.cpp
+++ src/backends/mpv/mpv_proxy.cpp
@@ -281,7 +281,17 @@ mpv_handle *MpvProxy::mpv_init()
             set_property(h, "hwdec", "vdpau");
             set_property(h, "vo", "vdpau");
         } else {
-            set_property(h, "vo", "gpu,x11,xv");
+            auto e = QProcessEnvironment::systemEnvironment();
+            QString XDG_SESSION_TYPE = e.value(QStringLiteral("XDG_SESSION_TYPE"));
+            QString WAYLAND_DISPLAY = e.value(QStringLiteral("WAYLAND_DISPLAY"));
+
+            if (XDG_SESSION_TYPE == QLatin1String("wayland") ||
+                    WAYLAND_DISPLAY.contains(QLatin1String("wayland"), Qt::CaseInsensitive)) {
+                set_property(h, "vo", "gpu,x11,xv");
+            } else {
+                set_property(h, "vo", "xv");
+            }
+
         }
 #else
         set_property(h, "vo", "gpu,xv,x11");
