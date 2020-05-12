60c73fe69e0aed3963f1cf81293085c185fe94fb
yangmin <yangmin@uniontech.com>
2020-05-12 10:48:40 +0800

    Title:fix
    Description:fix the problem that movie will be paused and do not resume
    playing state when press middle cursor in kelvinu
    
    RootCause:Null
    
    Solution:include out the middle press event to pause the movie playing
    to pauseed

--- src/common/mainwindow.cpp
+++ src/common/mainwindow.cpp
@@ -3566,6 +3566,7 @@ void MainWindow::keyReleaseEvent(QKeyEvent *ev)
 void MainWindow::capturedMousePressEvent(QMouseEvent *me)
 {
     _mouseMoved = false;
+    _mousePressed = false;
 #ifdef __aarch64__
     _nwComm->hide();
 #elif __mips__
@@ -3594,6 +3595,7 @@ static bool _afterDblClick = false;
 void MainWindow::mousePressEvent(QMouseEvent *ev)
 {
     _mouseMoved = false;
+    _mousePressed = false;
 #ifdef __aarch64__
     _nwComm->hide();
 #elif __mips__
