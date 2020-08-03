fd61a75ca0c63f6eb366ef736269df11ef65532a
xiangxiaojun <xiangxiaojun@uniontech.com>
2020-08-03 16:19:59 +0800

    fix:code merge from xcb to wayland
    Description:合并xcb最近的提交到wayland
    
    log:
    bug:
    task:

--- src/libdmr/playlist_model.h
+++ src/libdmr/playlist_model.h
@@ -41,6 +41,7 @@ namespace dmr {
 using namespace ffmpegthumbnailer;
 class PlayerEngine;
 class LoadThread;
+class GetThumanbil;
 
 struct MovieInfo {
     bool valid;
@@ -183,14 +184,17 @@ public:
     bool getthreadstate();
     void savePlaylist();
     void clearPlaylist();
+    QList<QUrl> getLoadList() {return m_loadFile;};
 
 public slots:
     void changeCurrent(int);
     void delayedAppendAsync(const QList<QUrl> &);
     void deleteThread();
+    void clearLoad();
 
 private slots:
     void onAsyncAppendFinished();
+    void onAsyncFinished();
 
 
 signals:
@@ -209,6 +213,7 @@ private:
     int _count {0};
     int _current {-1};
     int _last {-1};
+    bool _hasNormalVideo{false};
     PlayMode _playMode {PlayMode::OrderPlay};
     QList<PlayItemInfo> _infos;
 
@@ -230,8 +235,11 @@ private:
     QString _playlistFile;
 
     LoadThread *m_ploadThread;
+    GetThumanbil *m_getThumanbil {nullptr};
     QMutex *m_pdataMutex;
     bool m_brunning;
+    QList<QUrl> m_tempList;
+    QList<QUrl> m_loadFile;
 
     void reshuffle();
     void loadPlaylist();
@@ -258,6 +266,59 @@ private:
 
     QList<AppendJob> _pendingJob; // async job
     QSet<QString> _urlsInJob;  // url list
+};
+
+class GetThumanbil : public QThread
+{
+    Q_OBJECT
+public:
+    GetThumanbil(PlaylistModel *model, const QList<QUrl> &urls)
+    {
+        m_model = model;
+        m_urls = urls;
+        m_mutex = new QMutex;
+        m_itemMutex = new QMutex;
+    };
+    ~GetThumanbil()
+    {
+        m_stop = true;
+    };
+    QList<PlayItemInfo> getInfoList() {return m_itemInfo;}
+    void stop() {m_stop = true;};
+    void setUrls(QList<QUrl> urls) {
+        m_mutex->lock();
+        m_urls = urls;
+        m_mutex->unlock();
+    };
+    void clearItem() {
+        m_itemMutex->lock();
+        m_itemInfo.clear();
+        m_urls.clear();
+        m_itemMutex->unlock();
+    };
+
+    void run()
+    {
+        m_mutex->lock();
+        QList<QUrl> urls = m_urls;
+        m_mutex->unlock();
+        foreach (QUrl url, urls) {
+            QFileInfo info(url.path());
+            m_itemMutex->lock();
+            m_itemInfo.append(m_model->calculatePlayInfo(url, info, false));
+            m_itemMutex->unlock();
+            m_isFinished = true;
+            if (m_stop) break;
+        }
+    }
+private:
+    PlaylistModel *m_model;
+    QList<QUrl> m_urls;
+    QList<PlayItemInfo> m_itemInfo;
+    QMutex *m_mutex;
+    QMutex *m_itemMutex;
+    bool m_isFinished {true};
+    bool m_stop {false};
 };
 
 
