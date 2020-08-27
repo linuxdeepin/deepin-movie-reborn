3ee23ec99b33c3b05e2ef5c748a687e729f3e4cd
lz <405292855@qq.com>
2020-08-27 09:11:05 +0800

    Title:fix
    Description: 增加外部函数接口
    
    RootCause:Null
    
    Solution:

--- src/libdmr/playlist_model.cpp
+++ src/libdmr/playlist_model.cpp
@@ -1640,7 +1640,165 @@ void LoadThread::run()
         _pModel->delayedAppendAsync(_urls);
     }
 }
+#ifdef _LIBDMR_
+static int open_codec_context(int *stream_idx,
+                              AVCodecContext **dec_ctx, AVFormatContext *fmt_ctx, enum AVMediaType type)
+{
+    int ret, stream_index;
+    AVStream *st;
+    AVCodec *dec = NULL;
+    AVDictionary *opts = NULL;
+    ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
+    if (ret < 0) {
+        qWarning() << "Could not find " << av_get_media_type_string(type)
+                   << " stream in input file";
+        return ret;
+    }
 
+    stream_index = ret;
+    st = fmt_ctx->streams[stream_index];
+#if LIBAVFORMAT_VERSION_MAJOR >= 57 && LIBAVFORMAT_VERSION_MINOR <= 25
+    *dec_ctx = st->codec;
+    dec = avcodec_find_decoder((*dec_ctx)->codec_id);
+#else
+    /* find decoder for the stream */
+    dec = avcodec_find_decoder(st->codecpar->codec_id);
+    if (!dec) {
+        fprintf(stderr, "Failed to find %s codec\n",
+                av_get_media_type_string(type));
+        return AVERROR(EINVAL);
+    }
+    /* Allocate a codec context for the decoder */
+    *dec_ctx = avcodec_alloc_context3(dec);
+    if (!*dec_ctx) {
+        fprintf(stderr, "Failed to allocate the %s codec context\n",
+                av_get_media_type_string(type));
+        return AVERROR(ENOMEM);
+    }
+    /* Copy codec parameters from input stream to output codec context */
+    if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codecpar)) < 0) {
+        fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
+                av_get_media_type_string(type));
+        return ret;
+    }
+#endif
+
+    *stream_idx = stream_index;
+    return 0;
+}
+MovieInfo MovieInfo::parseFromFile(const QFileInfo &fi, bool *ok)
+{
+    struct MovieInfo mi;
+    mi.valid = false;
+    AVFormatContext *av_ctx = NULL;
+    int stream_id = -1;
+    AVCodecContext *dec_ctx = NULL;
+
+    if (!fi.exists()) {
+        if (ok) *ok = false;
+        return mi;
+    }
+
+    auto ret = avformat_open_input(&av_ctx, fi.filePath().toUtf8().constData(), NULL, NULL);
+    if (ret < 0) {
+        qWarning() << "avformat: could not open input";
+        if (ok) *ok = false;
+        return mi;
+    }
+
+    if (avformat_find_stream_info(av_ctx, NULL) < 0) {
+        qWarning() << "av_find_stream_info failed";
+        if (ok) *ok = false;
+        return mi;
+    }
+
+    if (av_ctx->nb_streams == 0) {
+        if (ok) *ok = false;
+        return mi;
+    }
+    if (open_codec_context(&stream_id, &dec_ctx, av_ctx, AVMEDIA_TYPE_VIDEO) < 0) {
+        if (open_codec_context(&stream_id, &dec_ctx, av_ctx, AVMEDIA_TYPE_AUDIO) < 0) {
+            if (ok) *ok = false;
+            return mi;
+        }
+    }
+
+    av_dump_format(av_ctx, 0, fi.fileName().toUtf8().constData(), 0);
+
+    mi.width = dec_ctx->width;
+    mi.height = dec_ctx->height;
+    auto duration = av_ctx->duration == AV_NOPTS_VALUE ? 0 : av_ctx->duration;
+    duration = duration + (duration <= INT64_MAX - 5000 ? 5000 : 0);
+    mi.duration = duration / AV_TIME_BASE;
+    mi.resolution = QString("%1x%2").arg(mi.width).arg(mi.height);
+    mi.title = fi.fileName(); //FIXME this
+    mi.filePath = fi.canonicalFilePath();
+    mi.creation = fi.created().toString();
+    mi.fileSize = fi.size();
+    mi.fileType = fi.suffix();
+
+    mi.vCodecID = dec_ctx->codec_id;
+    mi.vCodeRate = dec_ctx->bit_rate;
+    if (dec_ctx->framerate.den != 0) {
+        mi.fps = dec_ctx->framerate.num / dec_ctx->framerate.den;
+    } else {
+        mi.fps = 0;
+    }
+    if (mi.height != 0) {
+        mi.proportion = mi.width / mi.height;
+    } else {
+        mi.proportion = 0;
+    }
+
+    if (open_codec_context(&stream_id, &dec_ctx, av_ctx, AVMEDIA_TYPE_AUDIO) < 0) {
+        if (open_codec_context(&stream_id, &dec_ctx, av_ctx, AVMEDIA_TYPE_VIDEO) < 0) {
+            if (ok) *ok = false;
+            return mi;
+        }
+    }
+
+
+    mi.aCodeID = dec_ctx->codec_id;
+    mi.aCodeRate = dec_ctx->bit_rate;
+    mi.aDigit = dec_ctx->sample_fmt;
+    mi.channels = dec_ctx->channels;
+    mi.sampling = dec_ctx->sample_rate;
+
+    AVDictionaryEntry *tag = NULL;
+    while ((tag = av_dict_get(av_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)) != NULL) {
+        if (tag->key && strcmp(tag->key, "creation_time") == 0) {
+            auto dt = QDateTime::fromString(tag->value, Qt::ISODate);
+            mi.creation = dt.toString();
+            qDebug() << __func__ << dt.toString();
+            break;
+        }
+        qDebug() << "tag:" << tag->key << tag->value;
+    }
+
+    tag = NULL;
+    AVStream *st = av_ctx->streams[stream_id];
+    while ((tag = av_dict_get(st->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)) != NULL) {
+        if (tag->key && strcmp(tag->key, "rotate") == 0) {
+            mi.raw_rotate = QString(tag->value).toInt();
+            auto vr = (mi.raw_rotate + 360) % 360;
+            if (vr == 90 || vr == 270) {
+                auto tmp = mi.height;
+                mi.height = mi.width;
+                mi.width = tmp;
+            }
+            break;
+        }
+        qDebug() << "tag:" << tag->key << tag->value;
+    }
+
+
+    avformat_close_input(&av_ctx);
+    mi.valid = true;
+
+    if (ok) *ok = true;
+    return mi;
+}
+#endif
 }
 
 #include "playlist_model.moc"
