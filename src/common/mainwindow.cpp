b5079b2ccd43b1b31354102921d4aa53a391755d
zhuyuliang <zhuyuliang@uniontech.com>
2020-09-07 18:37:00 +0800

    fix: bug 46569
    Describtion: 未获取到鼠标焦点，重新激活窗口
    
    Log: 代码问题
    
    Bug: https://pms.uniontech.com/zentao/bug-view-46702.html
    Change-Id: I74b1d9318ee28843b2be66e518b2e4ca4fd399b5
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-movie-reborn/+/3919
    Reviewed-by: xiangxiaojun <xiangxiaojun@uniontech.com>
    Reviewed-by: xiepengfei <xiepengfei@uniontech.com>
    Reviewed-by: liuminghang <liuminghang@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liuzheng <liuzheng@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/common/mainwindow.cpp
+++ src/common/mainwindow.cpp
@@ -2327,6 +2327,7 @@ void MainWindow::requestAction(ActionFactory::ActionKind kd, bool fromUI,
             _animationlable->move(QPoint((width() - _animationlable->width()) / 2,
                                          (height() - _animationlable->height()) / 2));
         }
+        activateWindow();
         break;
     }
 
