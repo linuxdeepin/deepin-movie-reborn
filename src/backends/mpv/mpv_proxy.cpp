f826fc0b70581246b41220907f24366ac9519225
“xiepengfei” <xiepengfei@uniontech.com>
2020-09-09 10:12:08 +0800

    fix: Modify audio and video synchronization mode
    Description: Video synchronization to audio in Godson no graphics card
    mode
    
    Log: Optimize the playback effect without graphics card
    
    Change-Id: I6a26933361a8c7971b5719f75cf8d59b7424c916
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-movie-reborn/+/4076
    Reviewed-by: hujianbo <hujianbo@uniontech.com>
    Reviewed-by: shuwenzhi <shuwenzhi@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liuzheng <liuzheng@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/backends/mpv/mpv_proxy.cpp
+++ src/backends/mpv/mpv_proxy.cpp
@@ -324,6 +324,13 @@ mpv_handle *MpvProxy::mpv_init()
     qInfo() << __func__ << get_property(h, "vo").toString();
     qInfo() << __func__ << get_property(h, "hwdec").toString();
 
+
+#ifdef __mips__
+    if (!CompositingManager::get().hascard()) {
+        qInfo() << "修改音视频同步模式";
+        set_property(h, "video-sync", "display-resample");
+    }
+#endif
     //QLocale locale;
     QString strMovie = QObject::tr("Movie");
     /*if (locale.language() == QLocale::Chinese) { //获取系统语言环境
