b0df415b761e8af38f95931a8e303c3c3ca0d0ec
xiepengfei <xiepengfei@uniontech.com>
2021-01-27 13:48:55 +0800

    fix: 优化编码规范
    修改moviewidget类中相关编码规范
    
    Log: 优化编码规范 
    Change-Id: I66cfe508462aa7915ca57e88c89d8949dc2740be

--- src/widgets/moviewidget.cpp
+++ src/widgets/moviewidget.cpp
@@ -1,5 +1,35 @@
-#include "moviewidget.h"
-
+/*
+ * (c) 2017, Deepin Technology Co., Ltd. <support@deepin.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
+/**
+ *@file 这个文件是播放音乐时显示的窗口动画
+ */
 #include <QHBoxLayout>
 #include <QTimer>
 #include <QLabel>
@@ -8,19 +38,21 @@
 #include <QDesktopWidget>
 #include <QPainter>
 
+#include "moviewidget.h"
+
 #define DEFAULT_RATION (1.0f*1080/1920)     //背景图片比例
 #define INTERVAL 50                         //刷新间隔
 #define ROTATE_ANGLE 360/(1000*2.5/INTERVAL)  //2.5秒转一圈
 
 namespace dmr {
-
+/**
+ * @brief MovieWidget 播放动画显示窗口
+ * @param parent 父窗口
+ */
 MovieWidget::MovieWidget(QWidget *parent)
-    : DWidget(parent), m_nRotate(0), m_nWidthNote(0), m_state(PlayState::StateStop)
+    : DWidget(parent), m_nRotate(0), m_nWidthNote(0), m_state(PlayState::STATE_STOP)
 {
-    m_pLabMovie = nullptr;
-    m_pTimer = nullptr;
-    m_pHBoxLayout = nullptr;
-
+    initMember();
     m_pHBoxLayout = new QHBoxLayout(this);
     m_pHBoxLayout->setContentsMargins(QMargins(0, 0, 0, 0));
     setLayout(m_pHBoxLayout);
@@ -37,35 +69,47 @@ MovieWidget::MovieWidget(QWidget *parent)
     m_pTimer->setInterval(INTERVAL);
     connect(m_pTimer, &QTimer::timeout, this, &MovieWidget::updateView);
 }
-
+/**
+ * @brief startPlaying
+ * 开始播放时的槽函数
+ */
 void MovieWidget::startPlaying()
 {
-    if (m_state == PlayState::StateStop) {
+    if (m_state == PlayState::STATE_STOP) {
         m_nRotate = 0;
         show();
     }
 
     m_pTimer->start();
-    m_state = PlayState::StatePlaying;
+    m_state = PlayState::STATE_PLAYING;
 }
-
+/**
+ * @brief stopPlaying
+ * 停止播放时的槽函数
+ */
 void MovieWidget::stopPlaying()
 {
     m_pTimer->stop();
-    m_state = PlayState::StateStop;
+    m_state = PlayState::STATE_STOP;
     hide();
 }
-
+/**
+ * @brief pausePlaying
+ * 暂停播放时的槽函数
+ */
 void MovieWidget::pausePlaying()
 {
     m_pTimer->stop();
-    m_state = PlayState::StatePause;
+    m_state = PlayState::STATE_PAUSE;
 }
-
+/**
+ * @brief updateView
+ * 更新窗口函数
+ */
 void MovieWidget::updateView()
 {
     QPixmap pixmapBg;
-    float nRatio = 1.0f;
+    float fRatio = 1.0f;
     QRect rectDesktop;
     int nWidth = 0;
     int nHeight = 0;
@@ -90,16 +134,25 @@ void MovieWidget::updateView()
     if (1.0f * nHeight / nWidth < DEFAULT_RATION) {
 
         nWidth = static_cast<int>(nHeight / DEFAULT_RATION);
-        nRatio = nWidth * 2.0f / rectDesktop.width();
+        fRatio = nWidth * 2.0f / rectDesktop.width();
     } else {
         nHeight = static_cast<int>(nWidth * DEFAULT_RATION);
-        nRatio = nHeight * 2.0f / rectDesktop.height();
+        fRatio = nHeight * 2.0f / rectDesktop.height();
     }
 
-    QPixmap pixmapFinal = pixmapBg.scaled(static_cast<int>(pixmapBg.width() * nRatio),
-                                          static_cast<int>(pixmapBg.height() * nRatio), Qt::IgnoreAspectRatio,
+    QPixmap pixmapFinal = pixmapBg.scaled(static_cast<int>(pixmapBg.width() * fRatio),
+                                          static_cast<int>(pixmapBg.height() * fRatio), Qt::IgnoreAspectRatio,
                                           Qt::SmoothTransformation);
     m_pLabMovie->setPixmap(pixmapFinal);
+}
+/**
+ * @brief initMember 初始化成员变量
+ */
+void MovieWidget::initMember()
+{
+    m_pLabMovie = nullptr;
+    m_pTimer = nullptr;
+    m_pHBoxLayout = nullptr;
 }
 
 }
--- src/widgets/moviewidget.h
+++ src/widgets/moviewidget.h
@@ -1,3 +1,35 @@
+/*
+ * (c) 2017, Deepin Technology Co., Ltd. <support@deepin.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
+/**
+ *@file 这个文件是播放音乐时显示的窗口动画
+ */
 #ifndef MOVIEWIDGET_H
 #define MOVIEWIDGET_H
 
@@ -12,35 +44,62 @@ class QLabel;
 class QPixmap;
 
 namespace dmr {
-
+/**
+ * @brief The MovieWidget class
+ * 播放音乐时动画效果显示窗口类
+ */
 class MovieWidget: public DWidget
 {
     Q_OBJECT
 
     enum PlayState {
-        StatePlaying,
-        StatePause,
-        StateStop
+        STATE_PLAYING = 0,
+        STATE_PAUSE,
+        STATE_STOP
     };
 
 public:
+    /**
+     * @brief MovieWidget 播放动画显示窗口
+     * @param parent 父窗口
+     */
     MovieWidget(QWidget *parent = nullptr);
 
 public slots:
+    /**
+     * @brief startPlaying
+     * 开始播放时的槽函数
+     */
     void startPlaying();
+    /**
+     * @brief stopPlaying
+     * 停止播放时的槽函数
+     */
     void stopPlaying();
+    /**
+     * @brief pausePlaying
+     * 暂停播放时的槽函数
+     */
     void pausePlaying();
+    /**
+     * @brief updateView
+     * 更新窗口函数
+     */
     void updateView();
+    /**
+     * @brief initMember 初始化成员变量
+     */
+    void initMember();
 
 private:
-    QLabel *m_pLabMovie;
-    QTimer *m_pTimer;
-    QHBoxLayout *m_pHBoxLayout;
-    QPixmap m_pixmapBg;
-    QPixmap m_pixmapNote;
-    int m_nRotate;     //旋转角度
-    int m_nWidthNote;  //音符边长
-    PlayState m_state;
+    QLabel *m_pLabMovie;          ///显示动画的控件
+    QTimer *m_pTimer;             ///控制动画播放速度的时间
+    QHBoxLayout *m_pHBoxLayout;   ///窗口布局管理
+    QPixmap m_pixmapBg;           ///底下胶片的图像
+    QPixmap m_pixmapNote;         ///上层音符的图像
+    int m_nRotate;                ///旋转角度
+    int m_nWidthNote;             ///音符边长
+    PlayState m_state;            ///播放状态
 };
 
 }
