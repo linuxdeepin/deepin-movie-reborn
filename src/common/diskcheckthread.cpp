b395e72f8c96b1aa763f3735af65478ccf9e8bb3
Zhu Yuliang <zhuyuliang@uniontech.com>
2021-01-28 13:34:45 +0800

    chore: 代码规范修改
    Describtion: 修改diskcheckthread代码规范，统一代码规范
    
    Log: 代码规范修改
    Change-Id: I32640fb00332452466d106b14433bb3355f07857

--- src/common/diskcheckthread.cpp
+++ src/common/diskcheckthread.cpp
@@ -1,10 +1,38 @@
+/*
+ * (c) 2017, Deepin Technology Co., Ltd. <support@deepin.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
 #include "diskcheckthread.h"
 
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include </usr/include/linux/cdrom.h>
 #include <QFile>
-
 
 Diskcheckthread::Diskcheckthread()
 {
@@ -28,17 +56,15 @@ void Diskcheckthread::diskChecking()
     QString strDiskPath;
     QString strDiskName;
     bool bOpen = true;
-
     QList<QString> listDisk = m_mapDisk2Name.values();
-
     if (mountFile.open(QIODevice::ReadOnly) == false) {
         return;
     }
     do {
-        QString strLine = mountFile.readLine();
-        if ( strLine.indexOf("/dev/sr") != -1 || strLine.indexOf("/dev/cdrom") != -1) {
-            strDiskPath = strLine.split(" ").at(0);
-            strDiskName = strLine.split(" ").at(1);
+        QString sLine = mountFile.readLine();
+        if (sLine.indexOf("/dev/sr") != -1 || sLine.indexOf("/dev/cdrom") != -1) {
+            strDiskPath = sLine.split(" ").at(0);
+            strDiskName = sLine.split(" ").at(1);
 
             int nFd = open(strDiskPath.toLatin1(), O_RDWR | O_NONBLOCK);
             if (nFd && ioctl(nFd, CDROM_DRIVE_STATUS) == CDS_TRAY_OPEN) {
@@ -46,10 +72,10 @@ void Diskcheckthread::diskChecking()
             }
 
             if (!m_mapDisk2Name.contains(strDiskPath) && bOpen) {
-                m_mapDisk2Name.insert(strLine.split(" ").at(0), strLine.split(" ").at(1));
+                m_mapDisk2Name.insert(sLine.split(" ").at(0), sLine.split(" ").at(1));
             } else if (m_mapDisk2Name.contains(strDiskPath) && m_mapDisk2Name.value(strDiskPath) != strDiskName && bOpen) {
                 listDisk.removeOne(m_mapDisk2Name.value(strDiskPath));
-                m_mapDisk2Name.insert(strLine.split(" ").at(0), strLine.split(" ").at(1));
+                m_mapDisk2Name.insert(sLine.split(" ").at(0), sLine.split(" ").at(1));
             } else {
                 if (bOpen) {
                     listDisk.removeOne(strDiskName);
@@ -58,7 +84,7 @@ void Diskcheckthread::diskChecking()
                 }
             }
         }
-    } while (!mountFile.atEnd() );
+    } while (!mountFile.atEnd());
     mountFile.close();
 
     foreach (QString strDiskName, listDisk) {
--- src/common/diskcheckthread.h
+++ src/common/diskcheckthread.h
@@ -1,3 +1,32 @@
+/*
+ * (c) 2017, Deepin Technology Co., Ltd. <support@deepin.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
 #ifndef DISKCHECKTHREAD_H
 #define DISKCHECKTHREAD_H
 
@@ -5,24 +34,37 @@
 #include <QTimer>
 #include <QObject>
 
-class Diskcheckthread:public QObject
+/**
+ * @file 光盘监测线程，监测光盘状态
+ */
+class Diskcheckthread: public QObject
 {
     Q_OBJECT
 
 signals:
-    void diskRemove(QString strDiskName);
+    /**
+     * @file 光盘移除信号
+     * @param 移除的光盘名
+     */
+    void diskRemove(QString sDiskName);
+
 public:
     Diskcheckthread();
-
+    /**
+     * @file 启动计时器循环检测线程
+     */
     void start();
+    /**
+     * @file 停止定时器
+     */
     void stop();
 
 protected slots:
     void diskChecking();
 
 private:
-    QMap<QString, QString> m_mapDisk2Name;
-    QTimer m_timerDiskCheck;
+    QMap<QString, QString> m_mapDisk2Name; //光盘挂载路径和光盘挂载名的映射
+    QTimer m_timerDiskCheck;               //监测定时器
 };
 
 #endif // DISKCHECKTHREAD_H
