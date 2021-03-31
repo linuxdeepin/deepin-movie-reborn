c444a175ceaf3faa0d4e9d4e6759b54da549c3da
xiepengfei <xiepengfei@uniontech.com>
2021-03-31 10:40:11 +0800

    feat: 补充申威宏定义
    判断中补充申威判断
    
    Log: Fix some known issues
    Change-Id: Idb992936a2e73386d776ef8195fa566fffc8df73

--- src/libdmr/compositing_manager.cpp
+++ src/libdmr/compositing_manager.cpp
@@ -248,7 +248,7 @@ CompositingManager::~CompositingManager()
     m_pMpvConfig = nullptr;
 }
 
-#if defined (__mips__) || defined (__aarch64__)
+#if defined (__mips__) || defined (__aarch64__) || defined (__sw_64__)
 bool CompositingManager::hascard()
 {
     return m_bHasCard;
