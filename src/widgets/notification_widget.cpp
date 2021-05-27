14ecddfc66efa2e3262325a4b08a51fe41d45570
fengli <fengli@uniontech.com>
2021-05-27 17:26:12 +0800

    fix: bug81069
    Description: 修改左上角提示框透明度
    
    Bug: https://pms.uniontech.com/zentao/bug-view-81069.html
    
    Log: no
    Change-Id: I335c34121e14a807d7f8ec0ed7ec7294ab540032

--- src/widgets/notification_widget.cpp
+++ src/widgets/notification_widget.cpp
@@ -221,13 +221,13 @@ void NotificationWidget::paintEvent(QPaintEvent *pPaintEvent)
     painter.setRenderHint(QPainter::Antialiasing);
 
     bool bLight = (DGuiApplicationHelper::LightType == DGuiApplicationHelper::instance()->themeType());
-    QColor color = QColor(42, 42, 42);
-    QColor borderColor = QColor("#C0C6D4");
-    borderColor.setAlpha(25);
+
+    QColor color = QColor(42, 42, 42, 255 * 0.8);
+    QColor borderColor = QColor(0, 0, 0, 255 * 0.3);
 
     if (bLight) {
-        color = QColor(252, 252, 252);
-        borderColor = QColor(0, 0, 0, 25);
+        color = QColor(247, 247, 247, 255 * 0.8);
+        borderColor = QColor(0, 0, 0, 255 * 0.05);
     }
 
 #if defined (__aarch64__) || defined (__mips__)
@@ -236,40 +236,37 @@ void NotificationWidget::paintEvent(QPaintEvent *pPaintEvent)
         {
             QPainterPath painterPath;
             painterPath.addRoundedRect(rect(), static_cast<qreal>(fRadius), static_cast<qreal>(fRadius));
-            borderColor.setAlpha(255 * 0.05);
             painter.setPen(borderColor);
             painter.drawPath(painterPath);
         }
 
         QRect viewRect = rect().marginsRemoved(QMargins(1, 1, 1, 1));
         QPainterPath painterPath;
-        painterPath.addRoundedRect(viewRect, static_cast<qreal>(fRadius), static_cast<qreal>(fRadius));
-        color.setAlpha(255 * 0.6);
+        painterPath.addRoundedRect(rect(), static_cast<qreal>(fRadius), static_cast<qreal>(fRadius));
         painter.fillPath(painterPath, color);
 
         QFrame::paintEvent(pPaintEvent);
     } else {
-        if(bLight)
-            color.setNamedColor("#F7F7F7");
         color.setAlpha(255);
         painter.fillRect(rect(), color);
 
         if(bLight)
-            color.setNamedColor("#F2F2F2");
-        painter.setPen(color);
+            borderColor.setNamedColor("#F2F2F2");
+        else
+            borderColor.setNamedColor("#1C1C1C");
+        painter.setPen(borderColor);
         painter.drawRect(rect());
-
     }
 #else
     if (!CompositingManager::get().composited()) {
-        if(bLight)
-            color.setNamedColor("#F7F7F7");
         color.setAlpha(255);
         painter.fillRect(rect(), color);
 
         if(bLight)
-            color.setNamedColor("#F2F2F2");
-        painter.setPen(color);
+            borderColor.setNamedColor("#F2F2F2");
+        else
+            borderColor.setNamedColor("#1C1C1C");
+        painter.setPen(borderColor);
         painter.drawRect(rect());
     } else {
         painter.fillRect(rect(), Qt::transparent);
@@ -282,8 +279,7 @@ void NotificationWidget::paintEvent(QPaintEvent *pPaintEvent)
 
         QRect viewRect = rect().marginsRemoved(QMargins(1, 1, 1, 1));
         QPainterPath painterPath;
-        painterPath.addRoundedRect(viewRect, static_cast<qreal>(fRadius), static_cast<qreal>(fRadius));
-        color.setAlpha(255 * 0.6);
+        painterPath.addRoundedRect(rect(), static_cast<qreal>(fRadius), static_cast<qreal>(fRadius));
         painter.fillPath(painterPath, color);
 
         QFrame::paintEvent(pPaintEvent);
