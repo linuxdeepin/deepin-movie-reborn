05984cecc89921b6c776eb7d972842107e434b39
xiepengfei <xiepengfei@uniontech.com>
2022-04-19 14:55:56 +0800

    fix: 修复影院设置未保存问题
    补充配置文件路径
    
    Bug: https://pms.uniontech.com/bug-view-125507.html
    Log: 修复部分已知问题
    Change-Id: I4e40a887995498c0192a07270d748c31ce1c4a23

--- src/common/dmr_settings.cpp
+++ src/common/dmr_settings.cpp
@@ -57,6 +57,7 @@ Settings::Settings()
     : QObject(nullptr)
 {
     m_sConfigPath = DStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+    m_sConfigPath += "/config.conf";
     qInfo() << "configPath" << m_sConfigPath;
     QSettingBackend *pBackend = new QSettingBackend(m_sConfigPath);
     if(!CompositingManager::isMpvExists()) {
