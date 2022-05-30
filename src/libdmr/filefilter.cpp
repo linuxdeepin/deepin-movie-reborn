fa501d3d5f5949cb1548dfe6813cf6972c7271af
muyuankai <muyuankai@uniontech.com>
2022-05-30 14:05:50 +0800

    fix: 修复本地视频名称上包含{}时，影院无法播放视频
    修复本地视频名称上包含{}时，影院无法播放视频
    
    Bug: https://pms.uniontech.com/bug-view-135847.html
    Log: 调用ffmpeg接口检查本地视频文件是否可播放时，文件路径转换为本地路径,增加单元测试

--- src/libdmr/filefilter.cpp
+++ src/libdmr/filefilter.cpp
@@ -219,7 +219,7 @@ FileFilter::MediaType FileFilter::typeJudgeByFFmpeg(const QUrl &url)
 
     AVFormatContext *av_ctx = nullptr;
 
-    nRet = g_mvideo_avformat_open_input(&av_ctx, url.toString().toUtf8().constData(), nullptr, nullptr);
+    nRet = g_mvideo_avformat_open_input(&av_ctx, url.toLocalFile().toUtf8().constData(), nullptr, nullptr);
 
     if(nRet < 0)
     {
