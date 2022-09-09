28debc9e36554d031da64d58cd738b512ca4ef28
pengfeixx <63588879+pengfeixx@users.noreply.github.com>
2022-09-09 09:59:47 +0800

    fix: 修复修改音量未取消静音问题 (#214)
    取消静音时同步设置dbus
    
    Bug: https://pms.uniontech.com/bug-view-157681.html
    Log: 修复部分已知问题

--- src/widgets/volumeslider.cpp
+++ src/widgets/volumeslider.cpp
@@ -297,6 +297,7 @@ void VolumeSlider::volumeChanged(int nVolume)
 
     if (m_nVolume > 0 && m_bIsMute) {      //音量改变时改变静音状态
         changeMuteState(false);
+        setMute(false);
     }
 
     refreshIcon();
