08d53ecea84ed50e69e50411d26822ba90ac1191
xiepengfei <xiepengfei@uniontech.com>
2023-12-27 16:19:04 +0800

    fix: 适配凌久微显卡
    适配凌久微显卡
    
    Log: 修复部分已知问题

--- src/libdmr/compositing_manager.cpp
+++ src/libdmr/compositing_manager.cpp
@@ -184,6 +184,9 @@ CompositingManager::CompositingManager()
         }
     }
 
+    if (QFile::exists("/sys/bus/pci/drivers/ljmcore"))
+        _composited = false;
+
     //读取配置
     m_pMpvConfig = new QMap<QString, QString>;
     utils::getPlayProperty("/etc/mpv/play.conf", m_pMpvConfig);
