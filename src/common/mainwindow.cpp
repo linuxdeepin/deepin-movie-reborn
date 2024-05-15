05a5e616e42d2a8060976025358c6290f9c7804f
muyuankai <muyuankai@uniontech.com>
2024-05-15 20:18:48 +0800

    fix: app position movement logic.
    fix: 应用层检测到屏幕后对应用进行移动
    
    应用层检测到屏幕后对应用进行移动
    
    Bug: https://pms.uniontech.com/bug-view-252617.html
    Log: 应用层检测到屏幕后对应用进行移动
    
    v20 BUG 分支合一到v25主线
    
    Task: https://pms.uniontech.com/task-view-383479.html

--- src/common/mainwindow.cpp
+++ src/common/mainwindow.cpp
@@ -1246,6 +1246,20 @@ MainWindow::MainWindow(QWidget *parent)
                                          SLOT(slotProperChanged(QString, QVariantMap, QStringList)));
     qInfo() << "session Path is :" << path;
     connect(dynamic_cast<MpvProxy *>(m_pEngine->getMpvProxy()),&MpvProxy::crashCheck,&Settings::get(),&Settings::crashCheck);
+    if(utils::check_wayland_env()) {
+        connect(qApp, &QGuiApplication::screenRemoved, this, [=](){
+            QRect geoRect = geometry();
+#if QT_VERSION_MAJOR == 6
+            QRect deskRect = this->screen()->availableGeometry();
+#else
+            QRect deskRect = QApplication::desktop()->availableGeometry(geoRect.topLeft());
+#endif
+
+            if(!deskRect.intersects(geoRect)) {
+                move(deskRect.x(), deskRect.y());
+            }
+        });
+    }
     //解码初始化
    decodeInit();
 }
