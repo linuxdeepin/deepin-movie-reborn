b517f5af41699c4b66e75ab72579b2e39d702907
houchengqiu <houchengqiu@uniontech.com>
2024-05-29 11:18:17 +0800

    fix: Fix possible crashes caused by mouse release events
       Fix possible crashes caused by mouse release events
    
    Log: Fix possible crashes caused by mouse release events

--- src/common/mainwindow.cpp
+++ src/common/mainwindow.cpp
@@ -1769,6 +1769,8 @@ void MainWindow::menuItemInvoked(QAction *pAction)
 
 bool MainWindow::isActionAllowed(ActionFactory::ActionKind actionKind, bool fromUI, bool isShortcut)
 {
+    Q_ASSERT(m_pEngine);
+
     if (m_bInBurstShootMode) {
         return false;
     }
@@ -1838,6 +1840,9 @@ void MainWindow::requestAction(ActionFactory::ActionKind actionKind, bool bFromU
                                QList<QVariant> args, bool bIsShortcut)
 {
     qInfo() << "actionKind = " << actionKind << "fromUI " << bFromUI << (bIsShortcut ? "shortcut" : "");
+
+    Q_ASSERT(m_pToolbox);
+    Q_ASSERT(m_pCommHintWid);
 
     if (!m_pToolbox->getbAnimationFinash() || m_bStartAnimation) {
         return;
@@ -3052,6 +3057,8 @@ DSettingsDialog *MainWindow::initSettings()
 
 void MainWindow::play(const QList<QString> &listFiles)
 {
+    Q_ASSERT(m_pEngine);
+
     QList<QUrl> lstValid;
     QList<QString> lstDir;
     QList<QString> lstFile;
@@ -4082,7 +4089,7 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *ev)
 
     qInfo() << __func__ << "进入mouseReleaseEvent";
 
-    if (!insideResizeArea(ev->globalPos()) && !m_bMouseMoved && (m_pPlaylist->state() != PlaylistWidget::Opened)) {
+    if (!insideResizeArea(ev->globalPos()) && !m_bMouseMoved && m_pPlaylist && (m_pPlaylist->state() != PlaylistWidget::Opened)) {
         if (!insideToolsArea(ev->pos())) {
             m_delayedMouseReleaseTimer.start(120);
         } else {
