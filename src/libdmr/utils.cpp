b1ea8dc418ae29677d069e2d37de57d9aa1d3432
lichaofan <lichaofan@uniontech.com>
2025-09-23 20:24:33 +0800

    chore: Improve logging related to the configuration file play.conf.
    Enhance the logging related to play.conf to facilitate problem analysis.
    
    chore: 配置文件play.conf的日志完善。
    
    完善与play.conf相关的日志，方便问题分析。

--- src/libdmr/utils.cpp
+++ src/libdmr/utils.cpp
@@ -576,7 +576,7 @@ void getPlayProperty(const char *path, QMap<QString, QString> *&proMap)
     QFileInfo fi(path);
     if ((fi.exists() && fi.isFile()) && fi.isReadable()) {
         QFile file(path);
-        if (file.open(QIODevice::ReadOnly) | QIODevice::Text) {
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QByteArray t;
             int index = 0;
             while (!file.atEnd()) {
@@ -587,15 +587,18 @@ void getPlayProperty(const char *path, QMap<QString, QString> *&proMap)
                     QString temp = list.back();
                     temp = temp.mid(0, temp.length() - 1);//去除/n
                     proMap->insert(list.first(), temp);
+                    qInfo() << "Read config line: key:" << list.first() << "value:" << temp;
                 } else {
-                    qWarning() << __func__ << QString("config line:%1 has error ").arg(index);
+                    qWarning() << __func__ << QString("ERROR! config line: %1 (line No: %2)").arg(QString(t)).arg(index);
                     continue;
                 }
             }
+            file.close();
+        } else {
+            qWarning() << __func__ << "File open ERROR, file path:" << path;
         }
-        file.close();
     } else {
-        qWarning() << __func__ << "file path error!!!!!";
+        qWarning() << __func__ << "File not exist, file path:" << path;
     }
 }
 
