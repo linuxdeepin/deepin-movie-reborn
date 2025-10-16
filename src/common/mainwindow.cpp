539d1a62f05bf7deb0f891d9cc9f16d82eb15488
lichaofan <lichaofan@uniontech.com>
2025-10-16 11:01:44 +0800

    chore: Correction of some misspelled words.
    一些单词拼写错误的纠正。
    thumanbil => thumbnail

--- src/common/mainwindow.cpp
+++ src/common/mainwindow.cpp
@@ -1917,7 +1917,7 @@ void MainWindow::requestAction(ActionFactory::ActionKind actionKind, bool bFromU
         if (QDateTime::currentMSecsSinceEpoch() - m_pToolbox->getMouseTime() < 500) {
             return;
         }
-        if (m_pEngine->getplaylist()->items().isEmpty() && m_pEngine->getplaylist()->getThumanbilRunning()) {
+        if (m_pEngine->getplaylist()->items().isEmpty() && m_pEngine->getplaylist()->getThumbnailRunning()) {
             return;
         }
         //允许影院打开音乐文件进行播放
@@ -2010,7 +2010,7 @@ void MainWindow::requestAction(ActionFactory::ActionKind actionKind, bool bFromU
                     //last exit item without playing, because the playlist has not been
                     //loaded into that file, so adding a thread waiting here.
                     //TODO(xxxxp):It will cause direct opening of the cartoon? May need to optimize Model View
-                    while (m_pEngine->getplaylist()->getThumanbilRunning()) {
+                    while (m_pEngine->getplaylist()->getThumbnailRunning()) {
                         QCoreApplication::processEvents();
                     }
                     qInfo() << "playlist_pos: " << restore_pos << " current: " << m_pEngine->playlist().current();
